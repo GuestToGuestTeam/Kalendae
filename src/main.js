2e65919808a9e0e14d0d52048729e58d6e99e67c
Théotime Loiseau <theotime@guesttoguest.com>
2016-12-07 09:47:19 +0100

    Remove console log

--- src/main.js
+++ src/main.js
@@ -609,7 +609,6 @@ Kalendae.prototype = {
 				if (opts.dateClassMap[dateString]) klass.push(opts.dateClassMap[dateString]);
 
 				if (redrawDays) {
-					console.log('ttt');
 					$span.innerHTML = day.format(opts.dayNumberFormat);
 				}
 				$span.className = klass.join(' ');
